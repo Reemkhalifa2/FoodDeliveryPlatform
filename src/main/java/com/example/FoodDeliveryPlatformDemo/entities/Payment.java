d973fd8fa3daabb2fe54316723eab1f0d0c87ea6
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 14:06:45 +0400

    Add Payment entity

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Payment.java
@@ -0,0 +1,20 @@
+package com.example.FoodDeliveryPlatformDemo.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment extends BaseEntity{
+    private String paymentMethod;
+    private String status;
+    private Double amount;
+    private String transactionRef;
+    private Date processedAt;
+}
