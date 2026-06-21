fe5c52302e9452936548cc8e732d90d059091d4a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 15:10:31 +0400

    Add relationships to DeliveryDriver entity

--- src/main/java/com/example/FoodDeliveryPlatformDemo/entities/DeliveryDriver.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/DeliveryDriver.java
@@ -1,9 +1,13 @@
 package com.example.FoodDeliveryPlatformDemo.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Driver;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -17,5 +21,7 @@ public class DeliveryDriver extends Person{
     private Double currentLat; //Current latitude location of driver
     private Double currentLng; //Current longitude location of driver
     private Boolean isOnline;
+    @OneToMany
+    private List<Delivery> deliveries;
 
 }
