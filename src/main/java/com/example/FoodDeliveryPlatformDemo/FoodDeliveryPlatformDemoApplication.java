05de197677fec7d19ae73b87bac8c6ce21daf774
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 11:38:11 +0400

    Initialize Food Delivery Platform project with Maven setup and basic application structure

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/FoodDeliveryPlatformDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodDeliveryPlatformDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryPlatformDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryPlatformDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FoodDeliveryPlatformDemo/FoodDeliveryPlatformDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodDeliveryPlatformDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryPlatformDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
