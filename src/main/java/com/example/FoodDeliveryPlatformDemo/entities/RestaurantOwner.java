8dfe2d0371bd275aa4c0d5f2b2eece3bf9b17049
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 15:11:57 +0400

    Add relationships to RestaurantOwner entity

--- src/main/java/com/example/FoodDeliveryPlatformDemo/entities/RestaurantOwner.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/RestaurantOwner.java
@@ -1,9 +1,12 @@
 package com.example.FoodDeliveryPlatformDemo.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -11,4 +14,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RestaurantOwner extends Person{
     private String businessLicenseCode;
+    @OneToMany
+    private List<Restaurant> restaurants;
 }
