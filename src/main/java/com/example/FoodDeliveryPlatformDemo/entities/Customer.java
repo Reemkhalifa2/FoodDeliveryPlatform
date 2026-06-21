9e03d4a8b52f98c0d87630f8e3ca714df3a4a7ab
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 15:09:25 +0400

    Add customer to ComboMeal entity

--- src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Customer.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Customer.java
@@ -1,9 +1,13 @@
 package com.example.FoodDeliveryPlatformDemo.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,5 +16,15 @@ import lombok.NoArgsConstructor;
 public class Customer extends Person{
     private Integer loyaltyPoints;
     private String customerCode;
+
+    @OneToMany
+    private List<CustomerAddress> customerAddresses;
+    @OneToMany
+    private List<Order> orders;
+    @OneToMany
+    private List<Review> reviews;
+
+    @ManyToOne
+    private Customer customer;
 
 }
