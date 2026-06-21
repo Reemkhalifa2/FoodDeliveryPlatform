07a301d6ca2b02e4dfb403b541f9e997a2adb2a5
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 15:10:52 +0400

    Add relationships to Order entity

--- src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Order.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Order.java
@@ -1,9 +1,17 @@
 package com.example.FoodDeliveryPlatformDemo.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
-
+import java.util.List;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
 public class Order extends BaseEntity{
     private String orderCode;
     private Date orderDate;
@@ -13,4 +21,16 @@ public class Order extends BaseEntity{
     private Double discountAmount;
     private Double totalAmount;
     private String deliveryNotes;
+
+    @ManyToOne
+    private Customer customer;
+    @ManyToOne
+    private Restaurant restaurant;
+    @OneToMany
+    private List<OrderItem> orderItems;
+    @OneToOne
+    private Delivery delivery;
+    @OneToOne
+    private Payment payment;
+
 }
