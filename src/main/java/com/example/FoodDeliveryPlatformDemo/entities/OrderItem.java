f448cf489c279616ad086fc37f39d9d46a1aec4a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-21 13:09:58 +0400

    Add OrderItem entity

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/OrderItem.java
@@ -0,0 +1,17 @@
+package com.example.FoodDeliveryPlatformDemo.entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem extends BaseEntity{
+    private Integer quantity;
+    private Double unitPrice;
+    private Double totalPrice;
+    private String specialInstructions ;
+}
