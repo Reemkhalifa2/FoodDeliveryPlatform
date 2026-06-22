ba026cc7a4423d642127a28d7aca4fca88bc0056
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-22 10:18:09 +0400

    Add global exception handling

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/exceptions/GenericException.java
@@ -0,0 +1,7 @@
+package com.example.FoodDeliveryPlatformDemo.exceptions;
+
+public class GenericException extends RuntimeException{
+    public GenericException(String message){
+        super(message);
+    }
+}
