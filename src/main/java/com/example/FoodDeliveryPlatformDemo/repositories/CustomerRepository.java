0fc1e494a577016517c7cdb552551bcd02c00307
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-22 10:16:29 +0400

    Create Repository for customer

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/CustomerRepository.java
@@ -0,0 +1,23 @@
+package com.example.FoodDeliveryPlatformDemo.repositories;
+
+import com.example.FoodDeliveryPlatformDemo.entities.Customer;
+import lombok.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    @Query("SELECT C FROM CUSTOMER C WHERE C.email=:email AND C.isActive=true")
+    Customer findByEmail(@Param("email") String email);
+
+    @Query("SELECT C from CUSTOMER C WHERE C.loyaltyPoints=:loyaltyPoints AND C.isActive=true")
+    List<Customer>findByLoyaltyPointsGreaterThanEqual(@Param("loyaltyPoints") Integer loyaltyPoints);
+    @Query("SELECT c FROM Customer c WHERE c.createdDate BETWEEN :start AND :end")
+    List<Customer> findCustomersByDateRange(
+            @Param("start") Data start,
+            @Param("end") Date end
+    );
+}
