e458bc5080e90327d3771ed0564cd2bf318d7fbb
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-22 15:56:00 +0400

    Refactor Person class: remove redundant validation annotations

--- src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Person.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/entities/Person.java
@@ -10,18 +10,10 @@ import lombok.Data;
 @MappedSuperclass
 @Data
 public class Person extends BaseEntity{
-    @NotBlank(message = "FIRST NANE CANNOT BE BLANK.")
     private String firstName;
-    @NotBlank(message = "LAST NANE CANNOT BE BLANK.")
-    @Column(unique = true)
-    @NotBlank(message = "EMAIL CANNOT BE BLANK.")
-    @Email(message = "INVALID EMAIL")
+    private String lastName;
     private String email;
-    @NotBlank(message = "PHONE NUMBER CANNOT BE BLANK.")
-    @Pattern(
-            regexp = "^[79][0-9]{7}$",
-            message = "Phone number must be 8 digits and start with 7 or 9"
-    )    private String phone;
-    @NotBlank(message = "PASSWORD CANNOT BE BLANK")
+    private String phone;
     private String passwordHash;
+
 }
