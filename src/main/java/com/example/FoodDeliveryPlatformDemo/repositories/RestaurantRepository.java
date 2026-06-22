6f7d24c35f02b87a7af569105786037dff938ee1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-22 10:17:41 +0400

    Create Repository for Restaurant

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/RestaurantRepository.java
@@ -0,0 +1,31 @@
+package com.example.FoodDeliveryPlatformDemo.repositories;
+
+import com.example.FoodDeliveryPlatformDemo.entities.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+
+    @Query("SELECT r FROM Restaurant r where r.isActive = true AND r.cuisineType =:cuisineType")
+    List<Restaurant>findByCuisineTypeIgnoreCase(String cuisineType);
+
+    @Query("SELECT r FROM Restaurant r where r.isActive = true AND r.acceptingOrders = true")
+    List<Restaurant>findByAcceptingOrdersTrue();
+
+    @Query("SELECT r FROM Restaurant r WHERE r.isActive = true and c.deliveryFee <= fee")
+    List<Restaurant>findByDeliveryFeeLessThanEqual(@Param("fee") double fee);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.isActive = true AND r.restaurantOwner.id = :ownerId")
+    List<Restaurant>findByOwner(@Param("ownerId") Long ownerId);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.name LIKE %:keyword%")
+    List<Restaurant> findByNameContainingIgnoreCase(String keyword);
+
+
+
+
+
+}
