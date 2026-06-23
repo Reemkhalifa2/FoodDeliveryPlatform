3f1c9f9368cc94299bf339a3cfe00bfd3b7e3969
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 11:44:42 +0400

    Add ComboMealRequestDTO for handling combo meal requests

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/ComboMealRequestDTO.java
@@ -0,0 +1,33 @@
+package com.example.FoodDeliveryPlatformDemo.dto.request;
+
+import com.example.FoodDeliveryPlatformDemo.entities.ComboMeal;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ComboMealRequestDTO {
+    @NotBlank(message = "Combo name is required")
+    private String comboName;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Total price is required")
+    @DecimalMin(value = "0.0")
+    private Double totalPrice;
+
+    public static ComboMeal toEntity(ComboMealRequestDTO dto) {
+        ComboMeal comboMeal = new ComboMeal();
+
+        comboMeal.setComboName(dto.getComboName());
+        comboMeal.setDescription(dto.getDescription());
+        comboMeal.setTotalPrice(dto.getTotalPrice());
+        return comboMeal;
+    }
+
+
+}
