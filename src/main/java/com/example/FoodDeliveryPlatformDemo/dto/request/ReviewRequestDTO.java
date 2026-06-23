3b126d0d9c1513655e4f730f61246e4fa629930f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 15:03:55 +0400

    Add ReviewRequestDTO class for review submissions

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/ReviewRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.FoodDeliveryPlatformDemo.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class ReviewRequestDTO {
+    @NotNull
+    @Max(value = 5)
+    @Min(value = 5)
+    private Integer rating;
+    private String comment;
+
+}
