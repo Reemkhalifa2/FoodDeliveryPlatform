b27cc20c4d6753aa2b1cd1b67fc0b36d68a68b8b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 11:29:06 +0400

    add vlidation for menu item fields

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/MenuItemRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/MenuItemRequestDTO.java
@@ -1,17 +1,29 @@
 package com.example.FoodDeliveryPlatformDemo.dto.request;
 
 import com.example.FoodDeliveryPlatformDemo.entities.MenuItem;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class MenuItemRequestDTO {
+    @NotBlank(message = "Name is required")
     private String name;
     private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0")
     private Double price;
+
+    @NotNull(message = "Vegetarian flag is required")
     private Boolean isVegetarian;
+
+    @NotNull(message = "Calories is required")
+    @PositiveOrZero(message = "Calories must be 0 or more")
     private Double calories;
 
     public static MenuItem toEntity(MenuItemRequestDTO dto) {
