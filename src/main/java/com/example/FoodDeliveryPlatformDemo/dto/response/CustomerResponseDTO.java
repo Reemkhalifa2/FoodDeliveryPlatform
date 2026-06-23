354bd0360243c7ff7cc5deecadfdb51c0270b90f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 10:50:03 +0400

    Add CustomerResponseDTO class for customer data transfer

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/response/CustomerResponseDTO.java
@@ -0,0 +1,47 @@
+package com.example.FoodDeliveryPlatformDemo.dto.response;
+
+import com.example.FoodDeliveryPlatformDemo.entities.Customer;
+import com.example.FoodDeliveryPlatformDemo.entities.CustomerAddress;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerResponseDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Integer loyaltyPoints;
+    private String customerCode;
+    private List<CustomerAddressResponseDTO> customerAddressResponseDTOList;
+
+    public static CustomerResponseDTO toResponse(Customer customer) {
+        CustomerResponseDTO dto = new CustomerResponseDTO();
+        dto.setFirstName(customer.getFirstName());
+        dto.setLastName(customer.getLastName());
+        dto.setEmail(customer.getEmail());
+        dto.setPhone(customer.getPhone());
+        dto.setLoyaltyPoints(customer.getLoyaltyPoints());
+        dto.setCustomerCode(customer.getCustomerCode());
+        dto.setCustomerAddressResponseDTOList(CustomerAddressResponseDTO.toResponse(customer.getCustomerAddresses()));
+
+        return dto;
+    }
+
+    public static List<CustomerResponseDTO> toResponse(List<Customer> customers) {
+        List<CustomerResponseDTO> dtos = new ArrayList<>();
+        for (Customer entity : customers) {
+            dtos.add(toResponse(entity));
+        }
+        return dtos;
+    }
+
+}
