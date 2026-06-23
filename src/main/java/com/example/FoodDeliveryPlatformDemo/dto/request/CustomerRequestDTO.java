3eabb72de89647d9b72687cf25de3610bf15318b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 10:49:58 +0400

    Add CustomerRequestDTO class for customer data transfer

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/CustomerRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/CustomerRequestDTO.java
@@ -1,25 +1,23 @@
 package com.example.FoodDeliveryPlatformDemo.dto.request;
 
 import com.example.FoodDeliveryPlatformDemo.entities.Customer;
+import com.example.FoodDeliveryPlatformDemo.utilities.HelperUtils;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
-public class CustomerRequest extends PersonRequest {
+public class CustomerRequestDTO extends PersonDTO{
 
-
-    public static Customer toEntity(CustomerRequest dto){
+    public static Customer toEntity(CustomerRequestDTO dto) {
         Customer customer = new Customer();
         customer.setFirstName(dto.getFirstName());
         customer.setLastName(dto.getLastName());
         customer.setEmail(dto.getEmail());
         customer.setPhone(dto.getPhone());
         customer.setPasswordHash(dto.getPassword());
-        customer.setCreatedDate(new Date());
         return customer;
     }
-
 }
