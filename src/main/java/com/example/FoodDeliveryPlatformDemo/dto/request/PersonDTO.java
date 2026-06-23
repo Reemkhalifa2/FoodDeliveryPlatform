cb9e684c2c714ec1099cd055fb3e82a4e07c9e5f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 10:50:29 +0400

    Add PersonDTO class for user data transfer

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/PersonDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/PersonDTO.java
@@ -5,10 +5,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class PersonRequest {
+@NoArgsConstructor
+public class PersonDTO {
     @NotBlank(message = "First name cannot be blank.")
     private String firstName;
     @NotBlank(message = "Last name cannot be blank.")
