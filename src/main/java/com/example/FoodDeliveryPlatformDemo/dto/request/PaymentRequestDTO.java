69520a26ac7388de307bf72a4b6147958382b2a2
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 13:49:04 +0400

    Add PaymentRequestDTO class for payment processing

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/PaymentRequestDTO.java
@@ -0,0 +1,28 @@
+package com.example.FoodDeliveryPlatformDemo.dto.request;
+
+import com.example.FoodDeliveryPlatformDemo.entities.Payment;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class PaymentRequestDTO {
+    @NotBlank(message = "Payment method is required")
+    private String paymentMethod;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.0")
+    private Double amount;
+
+    public static Payment toEntity(PaymentRequestDTO dto) {
+        Payment payment = new Payment();
+
+        payment.setPaymentMethod(dto.getPaymentMethod());
+        payment.setAmount(dto.getAmount());
+
+        return payment;
+    }
+
+
+}
