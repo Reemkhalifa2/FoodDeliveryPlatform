d195f8987202a143860e455e493b5fdea1b0a85e
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 11:38:11 +0400

    Add MenuItemResponseDTO for menu item response handling

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/response/MenuItemResponseDTO.java
@@ -0,0 +1,39 @@
+package com.example.FoodDeliveryPlatformDemo.dto.response;
+
+import com.example.FoodDeliveryPlatformDemo.entities.MenuItem;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class MenuItemResponseDTO {
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean isVegetarian;
+    private Double calories;
+
+    public static MenuItemResponseDTO toResponse(MenuItem menuItem) {
+        MenuItemResponseDTO dto = new MenuItemResponseDTO();
+
+        dto.setName(menuItem.getName());
+        dto.setDescription(menuItem.getDescription());
+        dto.setPrice(menuItem.getPrice());
+        dto.setIsVegetarian(menuItem.getIsVegetarian());
+        dto.setCalories(menuItem.getCalories());
+
+        return dto;
+    }
+
+    public static List<MenuItemResponseDTO> toResponse(List<MenuItem> menuItems) {
+        List<MenuItemResponseDTO> dtos = new ArrayList<>();
+
+        for (MenuItem entity : menuItems) {
+            dtos.add(toResponse(entity));
+        }
+
+        return dtos;
+    }
+
+}
