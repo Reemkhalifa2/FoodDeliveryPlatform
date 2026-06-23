12250e56d06f9cfb995be1b73718c7b54c1b666f
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 10:55:06 +0400

    add toEntity method to cover dto to owner entity

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/RestaurantOwnerRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/RestaurantOwnerRequestDTO.java
@@ -1,5 +1,7 @@
 package com.example.FoodDeliveryPlatformDemo.dto.request;
 
+import com.example.FoodDeliveryPlatformDemo.entities.Customer;
+import com.example.FoodDeliveryPlatformDemo.entities.RestaurantOwner;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RestaurantOwnerRequestDTO extends PersonDTO {
     private String businessLicenseCode;
+
+    public static RestaurantOwner toEntity(RestaurantOwnerRequestDTO dto) {
+        RestaurantOwner restaurantOwner = new RestaurantOwner();
+        restaurantOwner.setFirstName(dto.getFirstName());
+        restaurantOwner.setLastName(dto.getLastName());
+        restaurantOwner.setEmail(dto.getEmail());
+        restaurantOwner.setPhone(dto.getPhone());
+        restaurantOwner.setPasswordHash(dto.getPassword());
+        restaurantOwner.setBusinessLicenseCode(dto.getBusinessLicenseCode());
+        return restaurantOwner;
+    }
 
 }
