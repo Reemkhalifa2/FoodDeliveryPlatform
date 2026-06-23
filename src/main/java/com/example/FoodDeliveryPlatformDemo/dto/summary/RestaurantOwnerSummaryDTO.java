008e07ac7cbbc9fedaa8cd59b35ed98ccf78b373
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 11:03:31 +0400

    Add RestaurantOwnerSummaryDTO for summarizing restaurant owner details

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/summary/RestaurantOwnerSummaryDTO.java
@@ -0,0 +1,20 @@
+package com.example.FoodDeliveryPlatformDemo.dto.summary;
+
+import com.example.FoodDeliveryPlatformDemo.entities.Customer;
+import com.example.FoodDeliveryPlatformDemo.entities.RestaurantOwner;
+import lombok.Data;
+
+@Data
+public class RestaurantOwnerSummaryDTO {
+    private String fullName;
+
+    public static RestaurantOwnerSummaryDTO toSummary( RestaurantOwner restaurantOwner) {
+        RestaurantOwnerSummaryDTO dto = new RestaurantOwnerSummaryDTO();
+        dto.setFullName(restaurantOwner.getFirstName()
+                + " "
+                + restaurantOwner.getLastName()
+        );
+        return dto;
+    }
+
+}
