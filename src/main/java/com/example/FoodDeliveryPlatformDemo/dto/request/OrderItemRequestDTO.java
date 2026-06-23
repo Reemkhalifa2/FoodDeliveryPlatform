8a2951d71b53a9b1b2dd5d1753ba974c158e9356
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-23 12:26:31 +0400

    Add OrderItemRequestDTO class for order item requests

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/OrderItemRequestDTO.java
@@ -0,0 +1,33 @@
+package com.example.FoodDeliveryPlatformDemo.dto.request;
+
+import com.example.FoodDeliveryPlatformDemo.entities.MenuItem;
+import com.example.FoodDeliveryPlatformDemo.entities.OrderItem;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class OrderItemRequestDTO {
+    @Min(value = 1)
+    private Integer quantity;
+    private String specialInstructions;
+    @NotNull
+    private MenuItemRequestDTO menuItemRequestDTO;
+
+    public static OrderItem toEntity(OrderItemRequestDTO dto) {
+        OrderItem orderItem = new OrderItem();
+
+        orderItem.setQuantity(dto.getQuantity());
+        orderItem.setSpecialInstructions(dto.getSpecialInstructions());
+
+        if (dto.getMenuItemRequestDTO() != null) {
+            orderItem.setMenuItem(
+                    MenuItemRequestDTO.toEntity(dto.getMenuItemRequestDTO())
+            );
+        }
+
+        return orderItem;
+    }
+
+
+}
