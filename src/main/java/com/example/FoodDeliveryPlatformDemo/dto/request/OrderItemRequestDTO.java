fafd7ee8df372f5276ce1bc660482b8a56979612
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 09:03:31 +0400

    fix bug

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/OrderItemRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/OrderItemRequestDTO.java
@@ -4,9 +4,13 @@ import com.example.FoodDeliveryPlatformDemo.entities.MenuItem;
 import com.example.FoodDeliveryPlatformDemo.entities.OrderItem;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemRequestDTO {
     @Min(value = 1)
     private Integer quantity;
