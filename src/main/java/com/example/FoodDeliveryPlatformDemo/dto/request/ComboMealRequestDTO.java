e1f8fa46ce0d9556cfe61ea435dc2b45368eea47
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 09:03:09 +0400

    fix bug

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/ComboMealRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/ComboMealRequestDTO.java
@@ -4,11 +4,13 @@ import com.example.FoodDeliveryPlatformDemo.entities.ComboMeal;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ComboMealRequestDTO {
     @NotBlank(message = "Combo name is required")
     private String comboName;
