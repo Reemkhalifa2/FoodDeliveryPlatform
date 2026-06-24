1d5bab92afc6360d702c46634f8e4306edb323d1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 15:17:26 +0400

    Add custom query to retrieve active RestaurantOwner by ID

--- src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/RestaurantOwnerRepository.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/RestaurantOwnerRepository.java
@@ -3,9 +3,12 @@ package com.example.FoodDeliveryPlatformDemo.repositories;
 import com.example.FoodDeliveryPlatformDemo.entities.Restaurant;
 import com.example.FoodDeliveryPlatformDemo.entities.RestaurantOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantOwnerRepository extends JpaRepository<RestaurantOwner, Integer> {
-
+    @Query("SELECT o FROM RestaurantOwner o WHERE o.isActive = true AND o.id=:id")
+    RestaurantOwner getById(@Param("id") Integer id);
 }
