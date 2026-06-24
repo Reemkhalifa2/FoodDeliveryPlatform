615829c943da58ffe01d201f6363d0c25077c5c8
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 09:03:51 +0400

    fix bug

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/ReviewRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/ReviewRequestDTO.java
@@ -4,10 +4,14 @@ import com.example.FoodDeliveryPlatformDemo.entities.Review;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewRequestDTO {
     @NotNull
     @Max(value = 5)
