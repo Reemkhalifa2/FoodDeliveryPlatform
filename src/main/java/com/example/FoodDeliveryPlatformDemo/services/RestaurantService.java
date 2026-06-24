54d39fe17e5686000a76bc870fb100e84333da95
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 15:17:47 +0400

    Add createRestaurant method to RestaurantService

--- src/main/java/com/example/FoodDeliveryPlatformDemo/services/RestaurantService.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/services/RestaurantService.java
@@ -1,9 +1,13 @@
 package com.example.FoodDeliveryPlatformDemo.services;
 
 import com.example.FoodDeliveryPlatformDemo.dto.request.RestaurantOwnerRequestDTO;
+import com.example.FoodDeliveryPlatformDemo.dto.request.RestaurantRequestDTO;
 import com.example.FoodDeliveryPlatformDemo.dto.response.RestaurantOwnerResponseDTO;
+import com.example.FoodDeliveryPlatformDemo.dto.response.RestaurantResponseDTO;
+import com.example.FoodDeliveryPlatformDemo.entities.Restaurant;
 import com.example.FoodDeliveryPlatformDemo.entities.RestaurantOwner;
 import com.example.FoodDeliveryPlatformDemo.exceptions.NullRequestBodyException;
+import com.example.FoodDeliveryPlatformDemo.exceptions.OwnerNotFoundException;
 import com.example.FoodDeliveryPlatformDemo.repositories.RestaurantOwnerRepository;
 import com.example.FoodDeliveryPlatformDemo.repositories.RestaurantRepository;
 import com.example.FoodDeliveryPlatformDemo.utilities.HelperUtils;
@@ -29,9 +33,30 @@ public class RestaurantService {
         if(HelperUtils.isNull(restaurantOwnerRequestDTO)){
             throw new NullRequestBodyException("Request body is null");
         }
-        RestaurantOwner restaurantOwner = restaurantOwnerRepository.save(RestaurantOwnerRequestDTO.toEntity(restaurantOwnerRequestDTO));
+        RestaurantOwner restaurantOwner = RestaurantOwnerRequestDTO.toEntity(restaurantOwnerRequestDTO);
         restaurantOwner.setIsActive(true);
         restaurantOwner.setCreatedDate(new Date());
+        restaurantOwnerRepository.save(restaurantOwner);
         return RestaurantOwnerResponseDTO.toResponse(restaurantOwner);
     }
+
+    public RestaurantResponseDTO createRestaurant(RestaurantRequestDTO dto, Integer ownerId){
+        RestaurantOwner restaurantOwner = restaurantOwnerRepository.getById(ownerId);
+        if(HelperUtils.isNull(restaurantOwner)){
+            throw new OwnerNotFoundException();
+        }
+        if(HelperUtils.isNull(dto)){
+            throw new NullRequestBodyException("Request body is null");
+        }
+        Restaurant restaurant = RestaurantRequestDTO.toEntity(dto);
+        restaurant.setIsActive(true);
+        restaurant.setCreatedDate(new Date());
+        restaurant.setRestaurantOwner(restaurantOwner);
+        restaurantOwner.getRestaurants().add(restaurant);
+        restaurantRepository.save(restaurant);
+        restaurantOwnerRepository.save(restaurantOwner);
+        return RestaurantResponseDTO.toResponse(restaurant);
+
+    }
+
 }
