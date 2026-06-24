bcd2499cb1ab2ddeecdd4e7f0b6f8ed3c443a36c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 11:55:15 +0400

    add end points to the controller

--- src/main/java/com/example/FoodDeliveryPlatformDemo/controllers/CustomerController.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package com.example.FoodDeliveryPlatformDemo.controllers;
 
 import com.example.FoodDeliveryPlatformDemo.dto.request.CustomerRequestDTO;
 import com.example.FoodDeliveryPlatformDemo.dto.response.CustomerResponseDTO;
+import com.example.FoodDeliveryPlatformDemo.exceptions.InvalidRequestException;
 import com.example.FoodDeliveryPlatformDemo.services.CustomerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,30 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers(){
         return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<CustomerResponseDTO> getById(@PathVariable Integer id) {
+        return ResponseEntity.ok(customerService.getById(id));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<CustomerResponseDTO> getByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(customerService.getByEmail(email));
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public ResponseEntity<String> deactivateCustomer(@PathVariable Integer id){
+        return ResponseEntity.ok(customerService.deactivateCustomer(id));
+    }
+
+    @PutMapping("/{id}/loyalty/add/{points}")
+    public ResponseEntity<CustomerResponseDTO> addLoyaltyPoints(@PathVariable Integer id , @PathVariable Integer points){
+        return ResponseEntity.ok(customerService.updateLoyaltyPoints(id,points));
+    }
+
+    @PutMapping("/{id}/loyalty/deduct/{points} ")
+    public ResponseEntity<CustomerResponseDTO> applyLoyaltyPenalty(@PathVariable Integer id , @PathVariable Integer points){
+        return ResponseEntity.ok(customerService.applyLoyaltyPenalty(id,points));
     }
 
 }
