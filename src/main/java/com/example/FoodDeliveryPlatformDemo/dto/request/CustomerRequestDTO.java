12e45772940f15ec95713fab3d12ecc50ef9a6b3
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 09:17:01 +0400

    Add customer address handling to CustomerRequestDTO

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/CustomerRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/CustomerRequestDTO.java
@@ -1,15 +1,19 @@
 package com.example.FoodDeliveryPlatformDemo.dto.request;
 
 import com.example.FoodDeliveryPlatformDemo.entities.Customer;
+import com.example.FoodDeliveryPlatformDemo.entities.CustomerAddress;
 import com.example.FoodDeliveryPlatformDemo.utilities.HelperUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+
 @Data
 @NoArgsConstructor
 public class CustomerRequestDTO extends PersonDTO{
+    CustomerAddressRequestDTO customerAddressRequestDTO;
 
     public static Customer toEntity(CustomerRequestDTO dto) {
         Customer customer = new Customer();
@@ -18,6 +22,11 @@ public class CustomerRequestDTO extends PersonDTO{
         customer.setEmail(dto.getEmail());
         customer.setPhone(dto.getPhone());
         customer.setPasswordHash(dto.getPassword());
+        if (customer.getCustomerAddresses() == null) {
+            customer.setCustomerAddresses(new ArrayList<>());
+        }
+        CustomerAddress address = CustomerAddressRequestDTO.toEntity(dto.getCustomerAddressRequestDTO());
+        customer.getCustomerAddresses().add(address);
         return customer;
     }
 }
