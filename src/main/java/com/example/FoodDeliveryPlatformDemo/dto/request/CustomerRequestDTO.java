0bc88b1a2b3749637759dac34d46fa9f1155d7f1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-06-24 10:24:09 +0400

    add null check

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/CustomerRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/CustomerRequestDTO.java
@@ -3,6 +3,7 @@ package com.example.FoodDeliveryPlatformDemo.dto.request;
 import com.example.FoodDeliveryPlatformDemo.entities.Customer;
 import com.example.FoodDeliveryPlatformDemo.entities.CustomerAddress;
 import com.example.FoodDeliveryPlatformDemo.utilities.HelperUtils;
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,20 +14,29 @@ import java.util.ArrayList;
 @Data
 @NoArgsConstructor
 public class CustomerRequestDTO extends PersonDTO{
-    CustomerAddressRequestDTO customerAddressRequestDTO;
+    @Nullable
+    private CustomerAddressRequestDTO customerAddressRequestDTO;
 
     public static Customer toEntity(CustomerRequestDTO dto) {
         Customer customer = new Customer();
+
         customer.setFirstName(dto.getFirstName());
         customer.setLastName(dto.getLastName());
         customer.setEmail(dto.getEmail());
         customer.setPhone(dto.getPhone());
         customer.setPasswordHash(dto.getPassword());
-        if (customer.getCustomerAddresses() == null) {
+
+        if (HelperUtils.isNull(customer.getCustomerAddresses())) {
             customer.setCustomerAddresses(new ArrayList<>());
         }
-        CustomerAddress address = CustomerAddressRequestDTO.toEntity(dto.getCustomerAddressRequestDTO());
-        customer.getCustomerAddresses().add(address);
+
+        if (HelperUtils.isNotNull(dto.getCustomerAddressRequestDTO()) ) {
+            CustomerAddress customerAddress =
+                    CustomerAddressRequestDTO.toEntity(dto.getCustomerAddressRequestDTO());
+
+            customer.getCustomerAddresses().add(customerAddress);
+        }
+
         return customer;
     }
 }
