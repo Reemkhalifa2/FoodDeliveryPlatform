3e314032c992376fc06ebb4c1ca1aab9462fe8d4
ReemALadawi <aladawi12reem6@gmail.com>
2026-06-25 10:19:19 +0400

    Add OrderItemRepository for managing OrderItem entities

---
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.example.FoodDeliveryPlatformDemo.repositories;
+
+import com.example.FoodDeliveryPlatformDemo.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem , Integer> {
+    @Query("SELECT oi FROM OrderItem oi WHERE oi.isActive =true AND oi.id=:id")
+    OrderItem getById(@Param("id") Integer id);
+}
