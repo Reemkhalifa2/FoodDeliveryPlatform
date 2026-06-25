32aaa4f5fec8905f93beefd9a207705784c20424
ReemALadawi <aladawi12reem6@gmail.com>
2026-06-25 14:13:20 +0400

    Create new End points

--- src/main/java/com/example/FoodDeliveryPlatformDemo/controllers/OrderController.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/controllers/OrderController.java
@@ -1,9 +1,18 @@
 package com.example.FoodDeliveryPlatformDemo.controllers;
 
+import com.example.FoodDeliveryPlatformDemo.OrderStatus;
+import com.example.FoodDeliveryPlatformDemo.dto.request.CorporateOrderRequestDTO;
+import com.example.FoodDeliveryPlatformDemo.dto.request.OrderItemRequestDTO;
+import com.example.FoodDeliveryPlatformDemo.dto.response.CorporateOrderResponseDTO;
+import com.example.FoodDeliveryPlatformDemo.dto.response.OrderResponseDTO;
 import com.example.FoodDeliveryPlatformDemo.services.OrderService;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("orders")
@@ -13,6 +22,78 @@ public class OrderController {
         this.orderService = orderService;
     }
     OrderService orderService;
+
+    @PostMapping("/customer/{customerId}/restaurant/{restaurantId}")
+    public ResponseEntity<OrderResponseDTO> createOrder(
+            @PathVariable Integer customerId,
+            @PathVariable Integer restaurantId) {
+
+        OrderResponseDTO response = orderService.createOrder(customerId, restaurantId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/{orderId}/items")
+    public ResponseEntity<OrderResponseDTO> addItemToOrder(
+            @PathVariable Integer orderId,
+            @RequestBody List<OrderItemRequestDTO> orderItemRequestDTOS) {
+
+        OrderResponseDTO response = orderService.addItemToOrder(orderId, orderItemRequestDTOS);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @DeleteMapping("/{id}/items/{itemId}")
+    public ResponseEntity<OrderResponseDTO> removeMenuItemFromOrder(@PathVariable Integer id, @PathVariable Integer itemId){
+        OrderResponseDTO response = orderService.removeMenuItemFromOrder(id, itemId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+
+    }
+    @PutMapping("/{id}/discount/{amount}")
+    public ResponseEntity<OrderResponseDTO> applyDiscount(@PathVariable Integer id, @PathVariable Double amount){
+        return ResponseEntity.ok(orderService.applyDiscount(id, amount));
+    }
+
+    @PutMapping("/{id}/confirm")
+    public ResponseEntity<OrderResponseDTO> confirmOrder(@PathVariable Integer id) {
+        return ResponseEntity.ok(orderService.confirmOrder(id));
+    }
+    @PutMapping("/{id}/status/{status}")
+    public ResponseEntity<OrderResponseDTO> UpdateOrderStatus(@PathVariable Integer id , @PathVariable OrderStatus status){
+        return ResponseEntity.ok(orderService.updateOrderStatus(id, status));
+    }
+
+    @PutMapping("/{id}/cancel")
+    public ResponseEntity<OrderResponseDTO> cancelOrder(@PathVariable Integer id){
+        OrderResponseDTO response = orderService.cancelOrder(id);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderResponseDTO> getById(@PathVariable Integer id){
+        return ResponseEntity.ok(orderService.getById(id));
+    }
+
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<List<OrderResponseDTO>> findByRestaurantIdAndStatus(@PathVariable Integer restaurantId){
+        return ResponseEntity.ok(orderService.findByRestaurantIdAndStatus(restaurantId));
+    }
+
+    @PostMapping("/corporate")
+    public ResponseEntity<CorporateOrderResponseDTO> placeCorporateOrder(@RequestBody CorporateOrderRequestDTO corporateOrderRequestDTO){
+        CorporateOrderResponseDTO responseDTO = orderService.placeCorporateOrder(corporateOrderRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
