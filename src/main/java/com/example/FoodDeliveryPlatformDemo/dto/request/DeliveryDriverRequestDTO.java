dccdecf56056c0166c2dfebbada131b1792de089
ReemALadawi <aladawi12reem6@gmail.com>
2026-06-26 21:38:08 +0400

    Add fields for active status, creation date, and driver code in DeliveryDriverRequestDTO

--- src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/DeliveryDriverRequestDTO.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/dto/request/DeliveryDriverRequestDTO.java
@@ -2,12 +2,15 @@ package com.example.FoodDeliveryPlatformDemo.dto.request;
 
 import com.example.FoodDeliveryPlatformDemo.entities.Customer;
 import com.example.FoodDeliveryPlatformDemo.entities.DeliveryDriver;
+import com.example.FoodDeliveryPlatformDemo.utilities.HelperUtils;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -23,13 +26,17 @@ public class DeliveryDriverRequestDTO extends PersonDTO{
 
     public static DeliveryDriver toEntity(DeliveryDriverRequestDTO dto) {
         DeliveryDriver deliveryDriver = new DeliveryDriver();
+        deliveryDriver.setIsActive(true);
+        deliveryDriver.setCreatedDate(new Date());
+        deliveryDriver.setIsOnline(true);
+        deliveryDriver.setDriverCode(HelperUtils.generateId("Driver-",4));
         deliveryDriver.setFirstName(dto.getFirstName());
         deliveryDriver.setLastName(dto.getLastName());
         deliveryDriver.setEmail(dto.getEmail());
         deliveryDriver.setPhone(dto.getPhone());
         deliveryDriver.setPasswordHash(dto.getPassword());
         deliveryDriver.setVehiclePlate(dto.getVehiclePlate());
-        deliveryDriver.setPasswordHash(dto.getVehicleType());
+        deliveryDriver.setVehicleType(dto.getVehicleType());
         return deliveryDriver;
     }
 
