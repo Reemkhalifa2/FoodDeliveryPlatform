a17afd17f6589602d6bf55df3ecd5848c1986815
ReemALadawi <aladawi12reem6@gmail.com>
2026-06-26 21:38:39 +0400

    update Delivery repository

--- src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/DeliveryRepository.java
+++ src/main/java/com/example/FoodDeliveryPlatformDemo/repositories/DeliveryRepository.java
@@ -1,14 +1,24 @@
 package com.example.FoodDeliveryPlatformDemo.repositories;
 
 import com.example.FoodDeliveryPlatformDemo.entities.Delivery;
+import com.example.FoodDeliveryPlatformDemo.entities.DeliveryDriver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
+
 @Repository
 
 public interface DeliveryRepository extends JpaRepository<Delivery , Integer> {
+    @Query("SELECT d FROM Delivery d WHERE d.id = :id AND d.isActive = true")
+    Delivery getByID(@Param("id") Integer id);
 
+    @Query("SELECT d FROM Delivery d WHERE d.deliveryDriver.id = :driverId AND d.isActive = true")
+    List<Delivery> getActiveDelivery(@Param("driverId") Integer driverId);
+
+    @Query("SELECT d FROM Delivery d WHERE d.deliveryDriver.id = :driverId AND d.status = :status AND d.isActive = true")
+    List<Delivery> getDeliveriesForDriver(@Param("driverId") Integer driverId, @Param("status") String status);
 }
